b6cd021f493738bfbb77413c1251290354b917b7
shruthi krishnapuram <shruthikrishnapuram@shruthis-MacBook-Air.local>
2021-09-12 16:46:35 -0400

    First commit

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by shruthi krishnapuram on 9/12/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
